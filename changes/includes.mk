12c6dd834e5d9cffbc48578a2b07fde5c23731ec
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2015-03-20 19:47:21 -0300

    fix(*): permissions and identation

--- changes/includes.mk
+++ changes/includes.mk
@@ -12,55 +12,55 @@ REGISTRY = $(shell echo $$DEV_REGISTRY)
 GIT_SHA = $(shell git rev-parse --short HEAD)
 
 ifndef REGISTRY_PORT
-  REGISTRY_PORT = 5000
+	REGISTRY_PORT = 5000
 endif
 
 ifndef IMAGE_PREFIX
-  IMAGE_PREFIX = deis/
+	IMAGE_PREFIX = deis/
 endif
 
 ifndef BUILD_TAG
-  BUILD_TAG = git-$(GIT_SHA)
+	BUILD_TAG = git-$(GIT_SHA)
 endif
 
 ifndef S3_BUCKET
-  S3_BUCKET = deis-updates
+	S3_BUCKET = deis-updates
 endif
 
 ifndef DEIS_NUM_INSTANCES
-  DEIS_NUM_INSTANCES = 3
+	DEIS_NUM_INSTANCES = 3
 endif
 
 define echo_cyan
-  @echo "\033[0;36m$(subst ",,$(1))\033[0m"
+	@echo "\033[0;36m$(subst ",,$(1))\033[0m"
 endef
 
 define echo_yellow
-  @echo "\033[0;33m$(subst ",,$(1))\033[0m"
+	@echo "\033[0;33m$(subst ",,$(1))\033[0m"
 endef
 
 check-docker:
-  @if [ -z $$(which docker) ]; then \
-    echo "Missing \`docker\` client which is required for development"; \
-    exit 2; \
-  fi
+	@if [ -z $$(which docker) ]; then \
+		echo "Missing \`docker\` client which is required for development"; \
+		exit 2; \
+	fi
 
 check-registry:
-  @if [ -z "$$DEV_REGISTRY" ]; then \
-    echo "DEV_REGISTRY is not exported, try:  make dev-registry"; \
-  exit 2; \
-  fi
+	@if [ -z "$$DEV_REGISTRY" ]; then \
+		echo "DEV_REGISTRY is not exported, try:  make dev-registry"; \
+	exit 2; \
+	fi
 
 check-deisctl:
-  @if [ -z $$(which deisctl) ]; then \
-    echo "Missing \`deisctl\` utility, please install from https://github.com/deis/deis"; \
-  fi
+	@if [ -z $$(which deisctl) ]; then \
+		echo "Missing \`deisctl\` utility, please install from https://github.com/deis/deis"; \
+	fi
 
 define check-static-binary
-  if file $(1) | grep -q "statically linked"; then \
-    echo -n ""; \
-  else \
-    echo "The binary file $(1) is not statically linked. Build canceled"; \
-    exit 1; \
-  fi
+	if file $(1) | grep -q "statically linked"; then \
+		echo -n ""; \
+	else \
+		echo "The binary file $(1) is not statically linked. Build canceled"; \
+		exit 1; \
+	fi
 endef
